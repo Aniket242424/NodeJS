2c73acd4bf9f894d8ccd3aed0fa3cc7ea83839ce
Aniket242424 <amahangade24@gmail.com>
2020-04-13 11:31:58 +0530

    Delete Node Modules

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@
 const request=require('request');
 
 const forecast=(latitude,longitude,callback)=>{
-    const url = 'https://api.darksky.net/forecast/1e53e07357a49a2b7406d3a81f79290c/'+latitude+','+longitude+'';
+    const url = 'https://api.darksky.net/forecast/1e53e07357a49a2b7406d3a81f79290c/'+latitude+','+longitude+'?units=si';
     request({url,json:true},(error,{body})=>{
       if(error){
         callback('unable to connect to weather services',undefined)
@@ -11,7 +11,7 @@ const forecast=(latitude,longitude,callback)=>{
         callback('unable to find location',undefined);
       }
       else{
-        callback(undefined,body.daily.data[0].summary + " .It is currently " + body.currently.temperature + " degrees out.There is " + body.currently.precipProbability + "% chances of rain")
+        callback(undefined,body.daily.data[0].summary + "It is currently " + body.currently.temperature + " degrees out.There is " + body.currently.precipProbability + "% chances of rain")
       }
     })
   }
